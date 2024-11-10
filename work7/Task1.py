26a95137ec2feb7bdb9fce7bdc75c01710f60c3e
Evgeniya Speranskaya <ye.speranskaya@mail.ru>
2024-11-10 09:14:31 +0500

    added task1

--- work7/Task1.py
+++ work7/Task1.py
@@ -1,1 +1,19 @@
-print("sdfsdf")
+# Используйте библиотеку `Pillow` или `opencv-python` для
+# загрузки изображения в форматах jpg и png. Если загружен
+# файл png, проверьте, содержит ли он прозрачные пиксели
+# и если да, то сделайте такие пиксели белыми
+# непрозрачными.
+
+import PIL
+from PIL import Image
+
+def change_png():
+    image_path = "../resources/dog.png"
+    image = Image.open(image_path)
+    if image.format == 'PNG':
+        if image.mode == 'RGBA':
+            image = image.convert('RGB')
+    image.save("../resources/processed_image.png")
+    print("Файл успешно обработан!")
+
+change_png()
